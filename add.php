0c9994a63f62bda347f790262a41cb1260fffaeb
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-21 22:06:14 +0300

    Delete add.php

--- add.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbconn3 = pg_connect("host=172.20.8.15 port=5432 dbname=st9901_11kp user=st9901 password=pwd9901");
-if (!$link) { 
-    printf("Невозможно подключиться к базе данных");
- } 
- echo "Вроде подключилось";
-?>
