e618d155c15b41f31d829f5ae014c8c395012c00
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-21 23:22:30 +0300

    Update test.php

--- test.php
+++ test.php
@@ -8,10 +8,6 @@ $port = "5432";
 try {
     $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
-    $pdo = new PDO($dsn, $user, $password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo 'Ok';
 }
 catch (PDOException $e) {
