112094e40510793b3ae6e9dce0ee2eb15ac58950
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-21 22:58:54 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
 <?php
-echo "Test connect";
+$host = "ec2-52-18-201-153.eu-west-1.compute.amazonaws.com";
+$user = "txbqslbmnzrozh";
+$password = "d9b28f2ef3fb10f7b9a1b08375fab0c770adae150712c294c58ebdaa9e96e7c5";
+$dbname = "dfohkfn3fl717";
+$port = "5432";
+
+try {
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+    $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
 ?>
