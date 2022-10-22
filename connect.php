30ed1cc5afae12df1b3289994b50e4f931e72b28
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-22 21:38:41 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,4 @@
 <?php
-global $conn;
-$host = "ec2-52-18-201-153.eu-west-1.compute.amazonaws.com";
-$user = "txbqslbmnzrozh";
-$password = "d9b28f2ef3fb10f7b9a1b08375fab0c770adae150712c294c58ebdaa9e96e7c5";
-$dbname = "dfohkfn3fl717";
-$port = "5432";
 
 $conn_string = "host=ec2-52-18-201-153.eu-west-1.compute.amazonaws.com port=5432 dbname=dfohkfn3fl717 user=txbqslbmnzrozh password=d9b28f2ef3fb10f7b9a1b08375fab0c770adae150712c294c58ebdaa9e96e7c5";
 $dbconn4 = pg_pconnect($conn_string);
@@ -12,4 +6,11 @@ if (!$dbconn4){
     echo 'Nooo(((';
 }
 echo 'Yeees)';
+
+$result = pg_query($dbconn4, "SELECT test FROM test");
+if (!$result) {
+  echo "Произошла ошибка.\n";
+  exit;
+}
+
 ?>
