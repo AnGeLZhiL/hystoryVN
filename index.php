eccedec7e39ba21af1d1ecfba33879ffaf26229f
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-23 21:46:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ header('Content-Type: application/json; charset=utf-8');
 require 'connect.php';
 require 'functions.php';
 
-$type = $_GET['q'];
+$q = $_GET['q'];
+
+$params = explode('/', $q);
+
+die(print_r($q));
 
 if ($type === 'users'){
     getUsers($dbconn4);
