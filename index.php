a74c0ef65ab66d506583d5f385ef4c9e821ce7e0
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-23 20:17:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,11 @@
 header('Content-Type: application/json; charset=utf-8');
 
 require 'connect.php';
+require 'functions.php';
 
 $type = $_GET['q'];
 
 if ($type === 'users'){
-    $users = pg_query($dbconn4, "SELECT * FROM users");
-    $usersList = [];
-    while ($user = pg_fetch_assoc($users)){
-        $usersList[] = $user;
-    }
-    echo json_encode($usersList);
+    getUsers($dbconn4);
 }
-
-
-
 ?>
