fae4bdc3d5a1911a98dc4928c7ede89278acbcaa
AnGeLZhiL <70762650+AnGeLZhiL@users.noreply.github.com>
2022-10-23 13:24:28 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type:json/application');
+header('Content-Type: application/json; charset=utf-8');
 
 $conn_string = "host=ec2-52-18-201-153.eu-west-1.compute.amazonaws.com port=5432 dbname=dfohkfn3fl717 user=txbqslbmnzrozh password=d9b28f2ef3fb10f7b9a1b08375fab0c770adae150712c294c58ebdaa9e96e7c5";
 $dbconn4 = pg_pconnect($conn_string);
